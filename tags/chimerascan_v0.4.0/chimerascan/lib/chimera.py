47e6aee255470784d7ded5631fccf260db42c321
matthew.iyer <matthew.iyer@a99da29d-6197-2707-3973-89323fa47910>
2011-07-03 13:37:15 +0000

    version 0.4.0 beta release

---
+++ tags/chimerascan_v0.4.0/chimerascan/lib/chimera.py
@@ -0,0 +1,322 @@
+'''
+Created on Jun 3, 2011
+
+@author: mkiyer
+'''
+from base import parse_string_none
+from sam import get_clipped_interval
+
+DISCORDANT_TAG_NAME = "XC"
+class DiscordantTags(object):
+    CONCORDANT_TX = 0
+    DISCORDANT_STRAND_TX = 1
+    CONCORDANT_GENE = 2
+    DISCORDANT_STRAND_GENE = 3
+    CONCORDANT_GENOME = 4
+    DISCORDANT_STRAND_GENOME = 5
+    DISCORDANT_GENE = 9
+    DISCORDANT_GENOME = 17
+
+ORIENTATION_TAG_NAME = "XD"
+class OrientationTags(object):
+    NONE = 0
+    FIVEPRIME = 1
+    THREEPRIME = 2
+
+def cmp_orientation(a,b):
+    if (a == OrientationTags.NONE) or (b == OrientationTags.NONE):
+        return True
+    return (a != b)
+
+# constants
+MULTIMAP_BINS = (1,2,4,8,16,32,64,128)
+CHIMERA_SEP = "|"
+
+# chimera types
+class ChimeraTypes(object):
+    INTERCHROMOSOMAL = "Interchromosomal"
+    OVERLAP_CONVERGE = "Overlapping_Converging"
+    OVERLAP_DIVERGE = "Overlapping_Diverging"
+    OVERLAP_SAME = "Overlapping_Same"
+    OVERLAP_COMPLEX = "Overlapping_Complex"
+    READTHROUGH = "Read_Through"
+    INTRACHROMOSOMAL = "Intrachromosomal"
+    ADJ_CONVERGE = "Adjacent_Converging"
+    ADJ_DIVERGE = "Adjacent_Diverging"
+    ADJ_SAME = "Adjacent_Same"
+    ADJ_COMPLEX = "Adjacent_Complex"
+    INTRA_COMPLEX = "Intrachromosomal_Complex"
+    UNKNOWN = "Undetermined"
+    #CHIMERA_INTRA_CONVERGE = "Intrachromosomal_Converging"
+    #CHIMERA_INTRA_DIVERGE = "Intrachromsomal_Diverging"
+    #CHIMERA_INTRA_SAME = "Intrachromosomal_Same"
+
+class DiscordantRead(object):
+    """
+    stores read alignment information needed to nominate 
+    chimeric transcripts
+
+    (this is a subset of what is kept in SAM file)
+    """
+    def __init__(self):
+        self.qname = ""
+        self.hit_index = -1
+        self.readnum = -1
+        self.seq = ""
+        self.tid = -1
+        self.pos = -1
+        self.aend = -1
+        self.clipstart = -1
+        self.clipend = -1
+        self.is_reverse = False
+        self.numhits = 0
+        self.mismatches = 0
+        self.discordant_type = 0
+        self.orientation = 0
+        self.is_spanning = False
+
+    @staticmethod
+    def from_read(r):
+        a = DiscordantRead()
+        a.qname = r.qname
+        a.hit_index = r.opt('HI')
+        a.readnum = 1 if r.is_read2 else 0
+        a.seq = r.seq
+        a.tid = r.rname
+        a.pos = r.pos
+        a.aend = r.aend
+        a.clipstart, a.clipend = get_clipped_interval(r)
+        a.is_reverse = r.is_reverse
+        a.numhits = r.opt('NH')
+        a.mismatches = r.opt('NM')
+        a.discordant_type = r.opt(DISCORDANT_TAG_NAME)
+        a.orientation = r.opt(ORIENTATION_TAG_NAME)
+        a.is_spanning = False
+        return a
+
+    @staticmethod
+    def from_list(fields):
+        a = DiscordantRead()
+        a.qname = fields[0]
+        a.hit_index = int(fields[1])
+        a.readnum = int(fields[2])
+        a.seq = fields[3]
+        a.tid = int(fields[4])
+        a.pos = int(fields[5])
+        a.aend = int(fields[6])
+        a.clipstart = int(fields[7])
+        a.clipend = int(fields[8])
+        a.is_reverse = True if int(fields[9]) == 1 else False
+        a.numhits = int(fields[10])
+        a.mismatches = int(fields[11])
+        a.discordant_type = int(fields[12])
+        a.orientation = int(fields[13])
+        a.is_spanning = True if int(fields[14]) == 1 else False
+        return a
+
+    def to_list(self):
+        return [self.qname, self.hit_index, self.readnum, self.seq, 
+                self.tid, self.pos, self.aend, self.clipstart, 
+                self.clipend, int(self.is_reverse), self.numhits, 
+                self.mismatches, self.discordant_type, 
+                self.orientation, int(self.is_spanning)]
+
+
+class ChimeraPartner(object):
+    NUM_FIELDS = 12
+    
+    def __init__(self):
+        self.gene_name = None
+        self.tx_name = None
+        self.start = 0
+        self.end = 0
+        self.strand = "."
+        self.exon_start_num = 0
+        self.exon_end_num = 0
+        self.inner_dist = 0
+        self.mismatches = 0
+        self.frac = 0.0
+        self.multimap_hist = []
+        self.weighted_cov = 0.0
+    
+    def __repr__(self):
+        return ("<%s(gene_name='%s',tx_name='%s')>" % 
+                (self.__class__.__name__, self.gene_name, self.tx_name))
+
+    @staticmethod
+    def from_list(fields):
+        p = ChimeraPartner()
+        p.gene_name = fields[0]
+        p.tx_name = fields[1]
+        p.start = int(fields[2])
+        p.end = int(fields[3])
+        p.strand = fields[4]
+        p.exon_start_num = fields[5]
+        p.exon_end_num = fields[6]
+        p.inner_dist = int(fields[7])
+        p.mismatches = int(fields[8])
+        p.frac = float(fields[9])
+        p.multimap_hist = map(int, fields[10].split(","))
+        p.weighted_cov = float(fields[11])
+        return p
+    
+    def to_list(self):
+        return [self.gene_name, self.tx_name, self.start, self.end, 
+                self.strand, self.exon_start_num, self.exon_end_num,
+                self.inner_dist, self.mismatches, self.frac, 
+                ",".join(map(str,self.multimap_hist)), self.weighted_cov]
+
+
+class Chimera(object):
+    FIELD_DELIM = "|"
+    PAIR_DELIM = "||" 
+    READ_DELIM = ";"
+
+    def __init__(self):
+        self.partner5p = None
+        self.partner3p = None
+        self.name = None
+        self.chimera_type = 0
+        self.distance = None
+        self.num_encomp_frags = 0
+        self.num_spanning_frags = 0
+        self.read1_sense_frags = 0
+        # junction information
+        self.breakpoint_name = None
+        self.breakpoint_homology_5p = 0
+        self.breakpoint_homology_3p = 0
+        # raw read information
+        self.encomp_read_pairs = []
+        self.spanning_reads = []       
+
+    @staticmethod
+    def from_list(fields):
+        c = Chimera()
+        # chimera partner information
+        c.partner5p = ChimeraPartner.from_list(fields[0:ChimeraPartner.NUM_FIELDS])
+        c.partner3p = ChimeraPartner.from_list(fields[ChimeraPartner.NUM_FIELDS:2*ChimeraPartner.NUM_FIELDS])
+        # chimera information
+        FIRSTCOL = 2*ChimeraPartner.NUM_FIELDS
+        c.name = fields[FIRSTCOL]
+        c.chimera_type = fields[FIRSTCOL+1]
+        c.distance = parse_string_none(fields[FIRSTCOL+2])
+        if c.distance is not None:
+            c.distance = int(c.distance)
+        c.num_encomp_frags = int(fields[FIRSTCOL+3])   
+        c.num_spanning_frags = int(fields[FIRSTCOL+4])   
+        c.read1_sense_frags = int(fields[FIRSTCOL+5])
+        # breakpoint information
+        c.breakpoint_name = parse_string_none(fields[FIRSTCOL+6])
+        c.breakpoint_homology_5p = parse_string_none(fields[FIRSTCOL+7])
+        if c.breakpoint_homology_5p is not None:
+            c.breakpoint_homology_5p = int(c.breakpoint_homology_5p)
+        c.breakpoint_homology_3p = parse_string_none(fields[FIRSTCOL+8])
+        if c.breakpoint_homology_3p is not None:
+            c.breakpoint_homology_3p = int(c.breakpoint_homology_3p)
+        # raw encompassing read information
+        encomp_reads_field = parse_string_none(fields[FIRSTCOL+9])
+        if encomp_reads_field is None:
+            c.encomp_read_pairs = []
+        for read_pair_fields in encomp_reads_field.split(c.READ_DELIM):
+            dreads = []
+            for read_fields in read_pair_fields.split(c.PAIR_DELIM):
+                dreads.append(DiscordantRead.from_list(read_fields.split(c.FIELD_DELIM)))
+            c.encomp_read_pairs.append(dreads)
+        # raw spanning read information
+        spanning_reads_field = parse_string_none(fields[FIRSTCOL+10])
+        if spanning_reads_field is not None:
+            for read_fields in spanning_reads_field.split(c.READ_DELIM):
+                c.spanning_reads.append(DiscordantRead.from_list(read_fields.split(c.FIELD_DELIM)))
+        return c
+
+    @staticmethod
+    def parse(line_iter):
+        for line in line_iter:
+            if line.startswith("#"):
+                continue            
+            fields = line.strip().split('\t')
+            yield Chimera.from_list(fields)
+
+    def to_list(self):
+        fields = []
+        fields.extend(self.partner5p.to_list())
+        fields.extend(self.partner3p.to_list())
+        fields.extend([self.name, self.chimera_type, self.distance,
+                       self.num_encomp_frags,
+                       self.num_spanning_frags,
+                       self.read1_sense_frags, 
+                       self.breakpoint_name,
+                       self.breakpoint_homology_5p, 
+                       self.breakpoint_homology_3p])
+        encomp_reads = []
+        for dreads in self.encomp_read_pairs:
+            r5p = self.FIELD_DELIM.join(map(str,dreads[0].to_list()))
+            r3p = self.FIELD_DELIM.join(map(str,dreads[1].to_list()))                        
+            pair_fields = self.PAIR_DELIM.join([r5p,r3p])
+            encomp_reads.append(pair_fields)
+        fields.append(self.READ_DELIM.join(encomp_reads))
+        spanning_reads = []
+        for dread in self.spanning_reads:
+            r = self.FIELD_DELIM.join(map(str,dread.to_list()))                        
+            spanning_reads.append(r)
+        if len(spanning_reads) == 0:
+            fields.append("None")
+        else:
+            fields.append(self.READ_DELIM.join(spanning_reads))
+        return fields
+
+    def get_weighted_cov(self):
+        """
+        weighted coverage is the number of reads supporting the
+        chimera divided by the number of alignments of the reads
+        such that multimapping reads will be assigned a fractional
+        weight
+        """
+        cov = 0.0  
+        qnames = set()
+        for dpair in self.encomp_read_pairs:
+            cov += 2.0 / (dpair[0].numhits + dpair[1].numhits)
+            qnames.add(dpair[0].qname)
+        for dr in self.spanning_reads:
+            if dr.qname not in qnames:
+                cov += 1.0 / dr.numhits
+        return cov
+
+    def get_num_frags(self):
+        qnames = set()
+        for pair in self.encomp_read_pairs:
+            qnames.add(pair[0].qname)
+        for dr in self.spanning_reads:
+            qnames.add(dr.qname)
+        return len(qnames)
+
+    def get_num_spanning_frags(self):
+        qnames = set()
+        for dr in self.spanning_reads:
+            qnames.add(dr.qname)
+        return len(qnames)  
+
+    def get_num_unique_positions(self):
+        """
+        calculates total number of unique reads alignment
+        positions supporting chimera
+        """
+        # find all unique alignment positions and read names
+        encomp_pos = set()
+        qnames = set()
+        for pair in self.encomp_read_pairs:
+            encomp_pos.add((pair[0].pos, pair[1].pos))
+            qnames.add(pair[0].qname)
+        # add spanning reads
+        spanning_pos = set()
+        for dr in self.spanning_reads:
+            if dr.qname not in qnames:
+                spanning_pos.add(dr.pos)
+        return len(encomp_pos) + len(spanning_pos)
+
+    def get_num_unique_spanning_positions(self):
+        pos = set()
+        for dr in self.spanning_reads:
+            pos.add(dr.pos)
+        return len(pos)
+
---
+++ tags/chimerascan_v0.4.0/chimerascan/pipeline/write_output.py
@@ -0,0 +1,127 @@
+'''
+Created on Jul 1, 2011
+
+@author: mkiyer
+
+chimerascan: chimeric transcript discovery using RNA-seq
+
+Copyright (C) 2011 Matthew Iyer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import logging
+import os
+import sys
+import operator
+import collections
+
+from chimerascan.lib.chimera import Chimera
+from chimerascan.lib import config
+from chimerascan.lib.gene_to_genome import build_gene_to_genome_map, \
+    build_tx_cluster_map, gene_to_genome_pos
+
+def get_chimera_groups(input_file, gene_file):
+    # build a lookup table to get gene clusters from transcript name    
+    tx_cluster_map = build_tx_cluster_map(open(gene_file))
+    # build a lookup table to get genome coordinates from transcript 
+    # coordinates
+    # TODO: can either group by exact breakpoint, or just by
+    # gene cluster
+    #tx_genome_map = build_gene_to_genome_map(open(gene_file))
+    # group chimeras in the same genomic cluster with the same
+    # breakpoint
+    cluster_chimera_dict = collections.defaultdict(lambda: [])
+    for c in Chimera.parse(open(input_file)):
+        # get cluster of overlapping genes
+        cluster5p = tx_cluster_map[c.partner5p.tx_name]
+        cluster3p = tx_cluster_map[c.partner3p.tx_name]
+        # get genomic positions of breakpoints
+        #coord5p = gene_to_genome_pos(c.partner5p.tx_name, c.partner5p.end-1, tx_genome_map)
+        #coord3p = gene_to_genome_pos(c.partner3p.tx_name, c.partner3p.start, tx_genome_map)
+        # add to dictionary
+        cluster_chimera_dict[(cluster5p,cluster3p)].append(c)
+        # TODO: use this grouping instead?
+        #cluster_chimera_dict[(cluster5p,cluster3p,coord5p,coord3p)].append(c)
+    for key,chimeras in cluster_chimera_dict.iteritems():
+        yield key,chimeras
+
+def get_best_coverage_chimera(chimeras):
+    stats = []
+    for c in chimeras:
+        stats.append((c.get_num_unique_spanning_positions(),
+                      c.get_weighted_cov(),
+                      c.get_num_frags(),
+                      c))
+    sorted_stats = sorted(stats, key=operator.itemgetter(0,1,2), reverse=True)
+    return sorted_stats[0][3]
+
+def write_output(input_file, output_file, index_dir):
+    gene_file = os.path.join(index_dir, config.GENE_FEATURE_FILE)
+    # build a lookup table to get genome coordinates from transcript 
+    # coordinates
+    tx_genome_map = build_gene_to_genome_map(open(gene_file))
+    # group chimera isoforms together
+    # TODO: requires reading all chimeras into memory
+    lines = []
+    for key,chimeras in get_chimera_groups(input_file, gene_file):
+        #cluster5p,cluster3p,coord5p,coord3p = key
+        #cluster5p,cluster3p = key
+        txs5p = ",".join(set(c.partner5p.tx_name for c in chimeras))
+        txs3p = ",".join(set(c.partner3p.tx_name for c in chimeras))
+        genes5p = ",".join(set(c.partner5p.gene_name for c in chimeras))
+        genes3p = ",".join(set(c.partner3p.gene_name for c in chimeras))
+        c = get_best_coverage_chimera(chimeras)
+        # get genomic positions of breakpoints
+        chrom5p,strand5p,pos5p = gene_to_genome_pos(c.partner5p.tx_name, c.partner5p.end-1, tx_genome_map)
+        chrom3p,strand3p,pos3p = gene_to_genome_pos(c.partner3p.tx_name, c.partner3p.start, tx_genome_map)
+        fields = [chrom5p, pos5p, "+" if (strand5p == 0) else "-",
+                  chrom3p, pos3p, "+" if (strand3p == 0) else "-",
+                  txs5p, txs3p, genes5p, genes3p,
+                  c.chimera_type, c.distance,
+                  c.get_weighted_cov(),
+                  c.get_num_frags(),
+                  c.get_num_spanning_frags(),
+                  c.get_num_unique_positions(),
+                  c.get_num_unique_spanning_positions()]
+        lines.append(fields)
+    # sort
+    lines = sorted(lines, key=operator.itemgetter(16, 12, 13), reverse=True)    
+    f = open(output_file, "w")
+    print >>f, '\t'.join(['#chrom5p', 'breakpoint_pos_5p', 'strand5p',
+                          'chrom3p', 'breakpoint_pos_3p', 'strand3p',
+                          'transcript_ids_5p', 'transcript_ids_3p',
+                          'genes5p', 'genes3p',
+                          'type', 'distance',
+                          'multimap_weighted_cov',
+                          'total_frags', 'spanning_frags',
+                          'unique_alignment_positions',
+                          'unique_spanning_alignment_positions'])
+    for fields in lines:
+        print >>f, '\t'.join(map(str, fields))
+    f.close()
+    return config.JOB_SUCCESS
+
+def main():
+    from optparse import OptionParser
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    parser = OptionParser("usage: %prog [options] <index_dir> <in.txt> <out.txt>")
+    options, args = parser.parse_args()
+    index_dir = args[0]
+    input_file = args[1]
+    output_file = args[2]
+    return write_output(input_file, output_file, index_dir)
+
+if __name__ == "__main__":
+    sys.exit(main())
